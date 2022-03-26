ac7dfe254e63246e1cd97834a4ff1c36cbb023b8
glenchiridza <glenchiridz@gmail.com>
2022-03-26 19:27:56 +0200

    details page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Detail() {
   return (
-    <div>
+    <Container>
         
-    </div>
+        <Background>
+            <img src='/images/slider-badag.jpg' />
+        </Background>
+
+    </Container>
   )
 }
 
 export default Detail;
+
+const Container = styled.div`
+    min-height:calc(100vh - 70px);
+    padding: 0 (3.5vw + 5px);
+    position:relative;
+
+`;
+
+const Background = styled.div`
+    position:fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    
+`;
