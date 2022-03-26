0a20105c8f2ee4da6d7c9182d98c49ad3e507e22
glenchiridza <glenchiridz@gmail.com>
2022-03-26 15:30:25 +0200

    home component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div>Header</div>
+    <Container>
+        Header
+    </Container>
   )
 }
 
 export default Header;
+
+const Container = styled.div`
+    height:70px;
+    background-color:#090b13;
+`;
