59c18e7678cc95dfab0438b8f5d38d08b9563bac
glenchiridza <glenchiridz@gmail.com>
2022-03-26 18:22:12 +0200

    company viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -7,6 +7,23 @@ function Viewers() {
         <Wrap>
             <img src='images/viewers-disney.png'/>
         </Wrap>
+
+        <Wrap>
+            <img src='images/viewers-disney.png'/>
+        </Wrap>
+
+        <Wrap>
+            <img src='images/viewers-disney.png'/>
+        </Wrap>
+
+        <Wrap>
+            <img src='images/viewers-disney.png'/>
+        </Wrap>
+
+        <Wrap>
+            <img src='images/viewers-disney.png'/>
+        </Wrap>
+        
     </Container>
   )
 }
@@ -16,9 +33,16 @@ export default Viewers;
 const Container = styled.div`
     margin-top:35px;
     display:grid;
-    grid-template-columns:
+    grid-gap:20px;
+    grid-template-columns: repeat(5,minmax(0, 1fr));
 `;
 
 const Wrap = styled.div`
-    
+    border:3px solid rgba(249,249,249,0.2);
+    border-radius:10px;
+    img{
+        width:100%;
+        height:100%;
+        object-fit:cover;
+    }
 `;
