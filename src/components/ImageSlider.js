8b5bf9454a17d93f559cec5425de39ef04c6ea1f
glenchiridza <glenchiridz@gmail.com>
2022-03-26 17:01:56 +0200

    slick carousel

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+function ImageSlider() {
+
+  return (
+    <div>ImageSlider</div>
+  )
+}
+
+export default ImageSlider;
