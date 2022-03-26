c9bb10ded1fe725145bb88cd460522a9156ca1e4
glenchiridza <glenchiridz@gmail.com>
2022-03-26 18:58:26 +0200

    movie content done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -13,16 +13,18 @@ function Movies() {
             </Wrap>
 
             <Wrap>
-                <img src='/images/slider-scale.jpg' />
+                <img src='/images/slider-badag.jpg' />
             </Wrap>
+
 
             <Wrap>
                 <img src='/images/slider-scale.jpg' />
             </Wrap>
 
             <Wrap>
-                <img src='/images/slider-scale.jpg' />
+                <img src='/images/slider-badag.jpg' />
             </Wrap>
+
 
         </Content>
     </Container>
@@ -32,25 +34,34 @@ function Movies() {
 export default Movies;
 
 const Container = styled.div`
-
+    margin-bottom:20px;
 `;
 
 const Content = styled.div`
+    overflow-y:hidden;
     display:grid;
     grid-gap:25px;
     grid-template-columns: repeat(4, minmax(0,1fr));
 `;
 
 const Wrap = styled.div`
+    min-height:150px;
+    max-height:150px;
     border-radius:10px;
     overflow:hidden;
     border:3ox solid #e1e1e1;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition:all 0.3sec;
 
     img{
         width:100%;
         height:100%;
         object-fit:cover;
+    }
+
+    &:hover{
+        transform:scale(1.05);
+        border:#e1e1e1;
     }
 `;
