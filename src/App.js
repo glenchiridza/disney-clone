0d1db1f0ad28ca1f9da8ad821bf0b91baa2825db
glenchiridza <glenchiridz@gmail.com>
2022-03-26 19:08:03 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-import {BrowserRouter,Route,Link,Router} from 'react-router-dom';
+import Detail from './components/Detail';
+import {BrowserRouter,Route,Link,Routes} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/detail' element={ <Detail /> } />
+          <Route path='/' element={<Home/>} />
+
+        </Routes>
+
+      </BrowserRouter>
+      
     </div>
   );
 }
