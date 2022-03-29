8e71efeda0ea98aabbd46d8665d64baa61d3a664
glenchiridza <glenchiridz@gmail.com>
2022-03-29 20:36:56 +0200

    import firebase

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/detail" element={ <Detail /> } />
           <Route path="/login" element={ <Login /> } />
 
