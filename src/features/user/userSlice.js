f04fcc19d8a2244ddd163c07f7bb30dae73a0063
glenchiridza <glenchiridz@gmail.com>
2022-03-29 22:34:08 +0200

    state signout

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,1 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    name:"",
+    email:"",
+    photo:""
+}
+
+const userSlice = createSlice({
+    name:"user",
+    initialState,
+    reducers: {
+        setUserLogin: (state, action) =>{
+            
+        },
+        setSignOut: (state) =>{
+            state.name = null;
+            state.email = null;
+            state.photo = null;
+        }
+    }
+})
